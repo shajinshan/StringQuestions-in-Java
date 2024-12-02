e38e269664db20f9a790a492042fb0dfa3a39e52
shajinshan <shajinshajin02@gmail.com>
2024-12-02 21:36:28 +0530

    ReverseEachWord

--- src/ReverseEachWord.java
+++ src/ReverseEachWord.java
@@ -1,5 +1,17 @@
 public class ReverseEachWord {
     public static void main(String[] args) {
 
+        String str ="ahs nij";
+
+        String[] words = str.split(" ");
+
+        StringBuilder sb =new StringBuilder();
+
+        for (String word : words){
+            StringBuilder rev = new StringBuilder(word).reverse();
+
+            sb.append(rev).append(" ");
+        }
+        System.out.println(sb.toString());
     }
 }
